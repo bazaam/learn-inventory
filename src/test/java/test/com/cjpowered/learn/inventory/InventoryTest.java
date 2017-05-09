159d2a9d0fc303e85024a8a8325f1011ed2dcf93
bazaam <krazykid4172>
2017-05-08 21:25:40 -0700

    multiple item order test

--- src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
+++ src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -134,5 +135,43 @@ public class InventoryTest {
     	// then
         assertTrue(actual.isEmpty());
               
+    }
+    @Test
+    public void orderAllNeededItems(){
+    	// given
+    	final int onHandItem1 = 10;
+    	final int needItem1 = 20;
+    	final int onHandItem2 = 7;
+    	final int needItem2 = 83;
+    	final StockedItem item1 = new StockedItem(needItem1);
+    	final StockedItem item2 = new StockedItem(needItem2);
+    	final LocalDate today = LocalDate.now();
+    	final InventoryDatabase db = new DatabaseTemplate(){
+    		@Override
+    		public List<Item> stockItems() {
+    			// TODO Auto-generated method stub
+    		return Arrays.asList(item1, item2);
+    		}
+    		@Override
+    		public int onHand(Item item){
+    			if (item.equals(item1))
+    				return onHandItem1;
+    			if (item.equals(item2))
+    				return onHandItem2;
+    			return -99;
+    		}
+    	};
+        final InventoryManager im = new AceInventoryManager(db);
+
+    	
+    	// when
+    	final List<Order> actual = im.getOrders(today);
+    	
+    	// then
+    	assertEquals(2, actual.size());
+    	assertEquals(needItem1 - onHandItem1, actual.get(0).quantity);
+    	assertEquals(needItem2 - onHandItem2, actual.get(1).quantity);
+    	assertEquals(item1, actual.get(0).item);
+    	assertEquals(item2, actual.get(1).item);
     }
 }
